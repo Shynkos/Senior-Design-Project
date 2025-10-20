b679f88b7c0e38fd52256abc7bcb3abca641da8d
Evie <emeckley@ufl.edu>
2025-10-20 19:54:24 -0400

    Add initial sprite for character and weapon
    Add run, jump, and idle animation for character and rudimentary gun sprite

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var animated_sprite = $AnimatedSprite2D
+
 # gravity and jump velocity
 @export var gravity = 2000.0
 @export var jump_vel = -800.0
@@ -32,4 +34,22 @@ func _physics_process(delta):
 		velocity.y = jump_vel
 	# register attacks
 	Attack();
-	move_and_slide()	
+	move_and_slide()
+	handle_movement_animation(direction)
+
+func handle_movement_animation(dir):
+	if on_ground:
+		if !velocity:
+			animated_sprite.play("idle")
+		if velocity:
+			animated_sprite.play("run")
+			toggle_flip_sprite(dir)
+	elif !on_ground:
+		animated_sprite.play("jump")
+
+# make character sprite face correct direction
+func toggle_flip_sprite(dir):
+	if dir == 1: # moving right
+		animated_sprite.flip_h = false
+	if dir == -1: # moving left
+		animated_sprite.flip_h = true
