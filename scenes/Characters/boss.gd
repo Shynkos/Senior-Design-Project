15af77e4c6bee1b386733aa61f263ea8228102f8
Evie <emeckley@ufl.edu>
2025-11-05 10:24:22 -0500

    Implement idle and move animations for boss
    Add sprites for idle and move animations and add animation handling to script

--- scenes/Characters/boss.gd
+++ scenes/Characters/boss.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var animated_sprite = $AnimatedSprite2D
 
 signal attack1
 var health = 100
@@ -13,7 +14,21 @@ func _physics_process(_delta):
 	velocity.x = direction * speed
 	pathfinding()
 	move_and_slide()
+	handle_movement_animation(direction)
 
+func handle_movement_animation(dir):
+	if !velocity:
+		animated_sprite.play("idle")
+	if velocity:
+		animated_sprite.play("move")
+		toggle_flip_sprite(dir)
+
+# make character sprite face correct direction
+func toggle_flip_sprite(dir):
+	if dir < 0: # moving left
+		animated_sprite.flip_h = false
+	if dir > 0: # moving right
+		animated_sprite.flip_h = true
 
 func pathfinding():
 	nav.target_position = Player.global_position
