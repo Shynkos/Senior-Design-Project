2f3c281b2f13f6b8a83bcf6691a2f56889bea8a6
Christopher Kostas <ckostas@ufl.edu>
2025-11-24 11:40:45 -0500

    Merged game over screen textures

--- scenes/Components/game_over_menu.gd
+++ scenes/Components/game_over_menu.gd
@@ -1,13 +1,8 @@
 extends CanvasLayer
 
 
-signal respawn
-
-
 func _on_respawn_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/Levels/RoomTut.tscn")
-	respawn.emit()
-	
 
 func _on_quit_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/Components/start_menu.tscn")
