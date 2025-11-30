2aa440fa297bbd4b74a7d2ef8222de9219ccb20b
Christopher Kostas <ckostas@ufl.edu>
2025-11-30 17:49:08 -0500

    Fixed respawn bug and health bar updating between scenes

--- scenes/Components/hud.gd
+++ scenes/Components/hud.gd
@@ -1,3 +1,7 @@
 extends CanvasLayer
 
 @onready var hearts = [$Health1, $Health2, $Health3, $Health4, $Health5, $Health6, $Health7]
+
+func _ready():
+	for i in range(0, PlayerGlobal.Health):
+		hearts[i].visible = true
